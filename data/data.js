23ca098b5ae721e4a356e54835d514cd0e7ebac2
Giorgio Ventura <gventuraagramonte@gmail.com>
2023-11-05 16:32:20 -0500

    add new configuration in dockerfile

---
+++ data/data.js
@@ -0,0 +1,90 @@
+export const data = [
+    {
+        id: 1,
+        name: {
+            first: 'John',
+            last: 'Smith'
+        },
+        total: 2795.95,
+        status: 'On Hold',
+        method: 'PayPal',
+        date: '15 minutes ago'
+    },
+    {
+        id: 2,
+        name: {
+            first: 'Chris',
+            last: 'Adams'
+        },
+        total: 1195.95,
+        status: 'Processing',
+        method: 'PayPal',
+        date: '23 minutes ago'
+    },
+    {
+        id: 3,
+        name: {
+            first: 'Sarah',
+            last: 'Smith'
+        },
+        total: 495.85,
+        status: 'Completed',
+        method: 'Visa',
+        date: '1 hour ago'
+    },
+    {
+        id: 4,
+        name: {
+            first: 'Joseph',
+            last: 'Shoo'
+        },
+        total: 150.45,
+        status: 'Processing',
+        method: 'Mastercard',
+        date: '1 hours ago'
+    },
+    {
+        id: 5,
+        name: {
+            first: 'Steve',
+            last: 'Harding'
+        },
+        total: 175.75,
+        status: 'On Hold',
+        method: 'PayPal',
+        date: '2 hour ago'
+    },
+    {
+        id: 6,
+        name: {
+            first: 'John',
+            last: 'Smith'
+        },
+        total: 2795.95,
+        status: 'On Hold',
+        method: 'PayPal',
+        date: '3 hour ago'
+    },
+    {
+        id: 7,
+        name: {
+            first: 'John',
+            last: 'Smith'
+        },
+        total: 2795.95,
+        status: 'Completed',
+        method: 'PayPal',
+        date: '3 hour ago'
+    },
+    {
+        id: 8,
+        name: {
+            first: 'John',
+            last: 'Smith'
+        },
+        total: 2795.95,
+        status: 'Completed',
+        method: 'PayPal',
+        date: '4 hour ago'
+    },
+]
